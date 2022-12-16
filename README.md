929c837309467ab91260857ae6d201c61151dda8
jiker.dev <1906612210@qq.com>
2022-12-17 00:40:10 +0800

    feature: 完成文档

--- README.md
+++ README.md
@@ -1,9 +1,58 @@
-# tiny_url
+# tiny-url 短链接服务
 
 ## 一、概述
 
-rust编写的短链接服务
+rust编写的简易短链接服务
 
-当前进度：<https://www.bilibili.com/video/BV1Pd4y1k7sD?p=5>
+参考视频：<https://www.bilibili.com/video/BV1Pd4y1k7sD>
 
-02:12
+## 二、怎样运行该项目？
+
+### 2.1 启动数据库
+
+如果没有shell执行环境，请忽略下面的启动脚本
+
+进入数据库操作目录
+
+```shell
+cd db
+```
+
+启动数据库
+
+```shell
+./start.sh
+```
+
+此时启动两个服务：
+
+- mysql的docker容器服务
+- phpmyadmin的docker容器服务
+
+打开 <http://localhost:8080> 查看数据库
+
+### 2.2 移除mysql服务
+
+```shell
+./shutdown.sh
+```
+
+执行脚本后，mysql和phpmyadmin两个容器将被销毁。
+
+### 2.3 运行服务
+
+```shell
+# 回到项目根目录
+cd ../
+# 执行cargo run
+cargo run
+```
+
+## 三、接口列表
+
+| 序号  | 路由      | 备注    |
+|-----|---------|-------|
+| 1   | /       |       |
+| 2   | /create | 创建短链接 |
+| 3   | /{code} | 访问短链接 |
+| 4   | /links  | 查询短链接 |
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,34 @@
-use actix_web::{App, HttpServer, web};
-use sqlx::mysql::MySqlPoolOptions;
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use settings::Settings;
+use sqlx::mysql::MySqlPoolOptions;
+use tera::{Context, Tera};
 
 mod api;
 mod settings;
+
+#[macro_use]
+extern crate lazy_static;
+lazy_static! {
+    pub static ref TEMPLATES: Tera = {
+        let tera = match Tera::new("templates/**/*") {
+            Ok(t) => t,
+            Err(e) => {
+                println!("Parsing errors: {}", e);
+                ::std::process::exit(1);
+            }
+        };
+        tera
+    };
+}
+
+#[get("/")]
+async fn index() -> impl Responder {
+    let content = "Generating a tiny code format url";
+    let mut data = Context::new();
+    data.insert("content", content);
+    let rendered = TEMPLATES.render("index.html", &data).unwrap();
+    HttpResponse::Ok().body(rendered)
+}
 
 #[actix_web::main]
 async fn main() -> Result<(), sqlx::Error> {
@@ -27,6 +52,7 @@ async fn main() -> Result<(), sqlx::Error> {
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(pool.clone()))
+            .service(index)
             .service(api::links::create_link)
             .service(api::links::get_all_links)
             .service(api::links::get_from_link)
